39289035fe4321c3bda17184100fc91edf920262
fasbender <alamgirfaisalbin@gmail.com>
2022-07-31 22:05:15 +0600

    new component added

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
+/* eslint-disable react/jsx-pascal-case */
 import Navbar from './components/Navbar';
 import Banner from './components/Banner';
 import Activities from './components/Activities';
 import Philosophy from './components/Philosophy';
+import Hotels_Apartments from './components/Hotels_Apartments';
 import './App.css';
 
 function App() {
@@ -11,6 +13,7 @@ function App() {
       <Banner/>
       <Activities/>
       <Philosophy/>
+      <Hotels_Apartments/>
     </div>
   );
 }
--- src/components/Hotels_Apartments.js
+++ src/components/Hotels_Apartments.js
@@ -1,8 +1,62 @@
 import React from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faStar, faStarHalf } from '@fortawesome/free-solid-svg-icons'
 
 const Hotels_Apartments = () => {
   return (
-    <div>Hotels_Apartments</div>
+    <div className='h-a'>
+      <h3>BEAUTIES</h3>
+        <h1>Hotels and Apartments</h1>
+        <div className='h-a-container'>
+            <div className='places'>
+              <img src={process.env.PUBLIC_URL + './images/Rectangle_9.png'} alt="" />
+              <div className='place-name'>
+                  <div>
+                    <h3>Reine</h3>
+                    <div className='rating'>
+                      <FontAwesomeIcon icon={faStar}/>
+                      <FontAwesomeIcon icon={faStar}/>
+                      <FontAwesomeIcon icon={faStar}/>
+                      <FontAwesomeIcon icon={faStar}/>
+                      <FontAwesomeIcon icon={faStar}/>
+                    </div>
+                  </div>
+                  <span>$720</span>
+              </div>
+            </div>
+            <div className='places'>
+              <img src={process.env.PUBLIC_URL + './images/Rectangle_10.png'} alt="" />
+              <div className='place-name'>
+                  <div>
+                    <h3>Norway</h3>
+                    <div className='rating'>
+                      <FontAwesomeIcon icon={faStar}/>
+                      <FontAwesomeIcon icon={faStar}/>
+                      <FontAwesomeIcon icon={faStar}/>
+                      <FontAwesomeIcon icon={faStar}/>
+                    </div>
+                  </div>
+                  <span>$350</span>
+              </div>
+            </div>
+            <div className='places'>
+              <img src={process.env.PUBLIC_URL + './images/Rectangle_11.png'} alt="" />
+              <div className='place-name'>
+                  <div>
+                    <h3>Bergen</h3>
+                    <div className='rating'>
+                      <FontAwesomeIcon icon={faStar}/>
+                      <FontAwesomeIcon icon={faStar}/>
+                      <FontAwesomeIcon icon={faStar}/>
+                      <FontAwesomeIcon icon={faStar}/>
+                      <FontAwesomeIcon icon={faStarHalf} />
+                    </div>
+                  </div>
+                  <span>$670</span>
+              </div>
+            </div>
+        </div>
+    </div>
   )
 }
 
